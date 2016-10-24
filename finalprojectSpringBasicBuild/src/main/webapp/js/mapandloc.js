fa641145f61e4d75edafa69c62c5cf17090baac9
MaryChes <chessie4907@gmail.com>
2016-10-24 16:53:55 -0400

    Updated and Improved Location and Direction Services

--- finalprojectSpringBasicBuild/src/main/webapp/js/mapandloc.js
+++ finalprojectSpringBasicBuild/src/main/webapp/js/mapandloc.js
@@ -28,7 +28,7 @@
 		    var index = 0;
 		    var marker;
 		    var map;
-
+		    var useraddress = document.getElementById("userAddress").value;
 		    //Variables for div injection
 		    var divdis = '" border: 1px solid; border-radius: 5px; margin-left: 10px; margin-top: 0px; background-color: white;"';
 		    var classname = '"locationItem"';
@@ -50,7 +50,12 @@
 	        
 	        //Start of display function
 	         function( data ) {
-	    	   $('#tableContainer').html("")
+	    	   $("#tableContainer").delegate("table", "click", function(){
+             newlocation = new google.maps.LatLng($(this).data("lat"), $(this).data("lng"));
+             //console.log(newlocation);
+             map.setCenter(newlocation);
+             map.setZoom(16);
+          });
 	          //Start of array loop (for each returned JSON array object)
 	          $.each(data, function(index, value){
 
@@ -71,10 +76,10 @@
 	                $('#tableContainer').append("<tr><td><div id=" + index + " style=" + divdis + " class=" + classname + "></div></td></tr>");                            
 	                    
 	                    //display all info from the above GET statement in DataTable
-	                    var tableid = '"locItemDisp"';
+	                  
 	                    
 	                    //Format instructions for DataTable entries
-	                    $("#"+index).html("<table id=" + tableid + "><tr><td><br><strong>" + data[index].nme + 
+	                    $("#"+index).html("<table data-lat='"+lat+"' data-lng='"+long+"'><tr><td><br><strong>" + data[index].nme + 
 	                                      "</strong><br>Location ID: " + data[index].agcid + "<br>" +
 	                                       data[index].adr1 + " " + 
 	                                       data[index].adr2 + "<br>" + 
@@ -84,11 +89,31 @@
 	                                       "Phone Number: " + data[index].phone1 + "<br>" + 
 	                                       "Fax Number" + data[index].fax + "<br></td><td>" + 
 	                                       "E-Mail: " + data[index].email + "<br>" + 
-	                                       "Web Address: " + data[index].weburl + "<br>" + 
-	                                       lat + " - " + long + "<br><br></td></tr></table>")
+	                                       "Web Address: " + data[index].weburl + "<br><br></td></tr></table>");
 	                        
+	                     //embeded map function that needs user location data and HUD service MDation data   
+                    var clickloc = data[index].adr1 + ' ' + data[index].adr2 + ' ' + data[index].city + ' ' + data[index].state_NME + ', ' + data[index].mailingzipcd;
+                    var contentString =  "<table data-lat='"+lat+"' data-lng='"+long+"'><tr><td><br><strong>" + data[index].nme + 
+                                      "</strong><br>Location ID: " + data[index].agcid + "<br>" +
+                                       data[index].adr1 + " " + 
+                                       data[index].adr2 + "<br>" + 
+                                       data[index].city + " " + 
+                                       data[index].state_NME + ", " + 
+                                       data[index].mailingzipcd + "<br>" + 
+                                       "Phone Number: " + data[index].phone1 + "<br>" + 
+                                       "Fax Number" + data[index].fax + "<br></td><td>" + 
+                                       "E-Mail: " + data[index].email + "<br>" + 
+                                       "Web Address: " + data[index].weburl + '<br><br>' +
+                                       '<button><a style="text-decoration: none; color: black;" href="https://www.google.com/maps/dir/'+ userAddress +'/' + clickloc + '" target="_blank">Get Directions</a></button></td></tr></table>';
+
+
+
+	                     var infowindow = new google.maps.InfoWindow({
+                         content: contentString
+                    	});
+
 	                    //Start of Google maps markers
-	                    marker = new google.maps.Marker({
+	                    var marker = new google.maps.Marker({
 	                     position: hudLatLong,
 
 	                      //Displyed results when hovering on marker(InfoWindow does not like to live here)
@@ -102,10 +127,16 @@
 	                          "E-Mail: " + data[index].email + "\n" +
 	                          "Web Address: " + data[index].weburl,
 	                      map: map                 
-	                    });//End of Google maps markers                      
-	              }//End of for 2 
+	                    });//End of Google maps markers
+
+
+	                   marker.addListener('click', function() {
+                      		infowindow.open(map, marker);
+                    	}); //infowindow click 
+
+	              };//End of for 2 
 	            };//End of for 1
-	          })//End of Array loop
+	          });//End of Array loop
 
 	          //Google Maps zoom control call
 	          map.fitBounds(bounds);    
